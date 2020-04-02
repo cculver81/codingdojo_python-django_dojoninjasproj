b9f6173cc622bda885535ea6a42ab4e6613d70f4
Cinque Culver <culver.ce@gmail.com>
2020-04-02 16:16:50 -0400

    Assignment completed. Previous error resolved

--- dojo_ninjas_app/models.py
+++ dojo_ninjas_app/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class Dojo(models.Model):
-    name = models.CharField(max_length=255),
-    city = models.CharField(max_length=255),
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
+    desc = models.TextField()
 
 class Ninja(models.Model):
-    dojo = models.ForeignKey(Dojo, related_name= 'dojos', on_delete=models.CASCADE),
-    first_name = models.CharField(max_length=255),
+    dojo = models.ForeignKey(Dojo, related_name= 'ninjas', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
